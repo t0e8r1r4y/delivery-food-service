a08abb6663f71434f0abba00cfd8a2e4574dd263
Terry <terryakishin0814@gmail.com>
2022-09-06 10:08:35 +0900

    port 변경 3000 -> 4000

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   // 해당 영역에서는 middleware function만 사용가능
-  await app.listen(3000);
+  await app.listen(4000);
 }
 bootstrap();
