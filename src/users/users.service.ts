4f881aea7fe1f07872698adf6d70ff9715dc1029
Terry <terryakishin0814@gmail.com>
2022-09-07 12:00:16 +0900

    user service trycatch 수정

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import { User } from "./entities/user.entity";
@@ -76,68 +76,82 @@ export class UsersService {
         return {ok:true, token: token};
     }
  
-    
-    // userId comming from token
-    // 특정 값만 수정하고 싶은데 {email, password} 이런식으로 코드를 작성하면 둘다 들어가야되서 하나는 undefined가 뜨게된다.
-    // not null constraint error
+    // TODO - 이메일 인증처리 로직 정리가 필요함.
+    @TryCatch('editProfile method Fail - ')
     async editProfile( 
-        userId:number, {email, password}: EditProfileInput
+        userId : number, { email, password }: EditProfileInput
     ) : Promise<EditProfileOutput>  {
-        try {
-            // update method는 DB에 쿼리만 보낼 뿐! -> save method를 써야한다.
-            // return this.users.update(userId, {... editProfileInput});
-            // save method는 이미 있는 메서드는 업데이트 한다.
-            const id : number = userId; // findOne에서는 entity에 포함된 컬럼과 동일한 명칭의 변수로 조회가 가능하다.
-            const user = await this.users.findOne({where:{id}});
-            if(email) {
-                user.email = email;
-                user.verified = false;
-                await this.Verifications.delete({user: {id: user.id}});
-                const verification = await this.Verifications.save(this.Verifications.create({user}));
-                this.mailService.sendVerificationEmail(user.email, verification.code);
-            }
-            if(password) {
-                user.password = password;
-            }
-            await this.users.save(user);
-            return { ok: true };
-        } catch (error) {
-            return { ok:false, error: '프로파일 업데이트가 불가능합니다.' };
-        }
-    }
 
-    // MAT userProfile in resolver
-    async findById( id : number ) : Promise<UserProfileOutput> {
-        try {
-            const user = await this.users.findOne( {where:{id}} );
-            if(user){
-                return { ok: true, user: user };
-            }
-            throw new Error(); // Todo - 수정이 필요한 부분
-        } catch (error) {
-            console.log(error);
-            return { ok: false, error: '사용자를 찾지 못했습니다.' };
+        if( !email && !password) {
+            throw new Error('수정할 내용이 없습니다.');
         }
-    }
 
-    async verifyEmail(code:string) : Promise<VerifyEmailOutput> {
-        try {
-            const verification = await this.Verifications.findOne(
-                { where: {code}, relations: ['user'] } // findOne Method는 하나의 인자만 받는다. {} 사용 주의
+        const user = await this.users.findOne({
+            where : {
+                id : userId,
+            }
+        });
+
+        if( !user ) {
+            throw new Error('수정할 사용자를 찾지 못하였습니다.');
+        }
+
+        if(email) {
+            user.email = email;
+            user.verified = false;
+            await this.Verifications.delete({
+                user: {
+                    id: user.id,
+                }
+            });
+            const verification = await this.Verifications.save(
+                this.Verifications.create({user}),
             );
-
-            if(verification){
-                verification.user.verified = true;
-                await this.users.save(verification.user);
-                await this.Verifications.delete(verification.id);
-                return { ok: true };
-            }
-            
-            return { ok: false, error: '인증값을 찾지 못했습니다.' };
-
-        } catch (error) {
-            console.log(error);
-            return { ok: false, error: '인증에 실패했습니다.' };
+            this.mailService.sendVerificationEmail(user.email, verification.code);
         }
+
+        if(password) {
+            user.password = password;
+        }
+
+        await this.users.save(user);
+
+        return { ok: true, };
+    }
+
+    @TryCatch('findById method Fail - ')
+    async findById( 
+        id : number 
+    ) : Promise<UserProfileOutput> {
+        const user = await this.users.findOne({
+            where : {
+                id: id,
+            }
+        })
+
+        if(!user) {
+            throw new Error(`${id}의 사용자를 찾을 수 없습니다.`);
+        }
+
+        return { ok: true, user: user };
+    }
+
+    @TryCatch('verifyEmail method Fail - ')
+    async verifyEmail(
+        code:string
+    ) : Promise<VerifyEmailOutput> {
+        const verification = await this.Verifications.findOne({
+            where: {code},
+            relations: ['user']
+        });
+
+        if(!verification) {
+            throw new NotFoundException('인증값을 찾지 못했습니다.');
+        }
+        verification.user.verified = true;
+        await this.users.save(verification.user);
+        await this.Verifications.delete(verification.id);
+
+        return { ok: true };
     }
 }
