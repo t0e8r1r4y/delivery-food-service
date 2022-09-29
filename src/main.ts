6a3cc075c618133d5a77baac4c6a92d91a70df24
Terry <terryakishin0814@gmail.com>
2022-09-30 00:26:16 +0900

    port 수정

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  const port = process.env.PORT;
-  await app.listen(port);
-  // await app.listen(4000);
+  const port = process.env.PORT || 4000;
+  await app.listen(port); // await app.listen(4000);
 }
 bootstrap();
